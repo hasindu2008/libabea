872fc04337bfd95b20e0c60098b5626986c905c2
Psy-Fer <fergo1988@gmail.com>
2020-03-04 18:53:54 +1100

    batching will will need to be handled

--- abea.pyx
+++ abea.pyx
@@ -11,8 +11,10 @@ from libabea cimport *
 
 np.import_array()
 
-def abea_python(read,  samples, digitisation, offset, range, sample_rate):
+def abea_python(read, samples, digitisation, offset, range, sample_rate):
 
+    # this won't work for batching, but code in motif2sig.py can be ported
+    # to support batching
     samples_floats = [float(i) for i in samples]
     cdef np.ndarray[np.float32_t,ndim=1] samples_array
     samples_array = np.ascontiguousarray(samples_floats, dtype=np.float32)
@@ -24,6 +26,9 @@ def abea_python(read,  samples, digitisation, offset, range, sample_rate):
     output.base_index = <int32_t *> malloc(sizeof(int32_t)*READ_LEN)
     output.raw_start_index = <int64_t *> malloc(sizeof(int64_t)*READ_LEN)
     output.raw_end_index = <int64_t *> malloc(sizeof(int64_t)*READ_LEN)
+
+    # code above this to create the batch of data as run_abea_on_batch with expect
+    # alternatively we can have 2 function calls, one for single, one for batch
 
     run_abea_on_read(<abea_out_t *> output, <int32_t> READ_LEN, <char *> read_array, <int64_t> samples_array.shape[0], <float *> samples_array.data, <float> digitisation, <float> offset, <float> range, <float> sample_rate, <int8_t> 0)
 
@@ -40,4 +45,3 @@ def abea_python(read,  samples, digitisation, offset, range, sample_rate):
     free(output)
 
     return ret
-
